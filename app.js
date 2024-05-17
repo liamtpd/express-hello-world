7ff48e9aaa93498b9aac61e74a29441a7ee82b39
liamtpd <101474535+liamtpd@users.noreply.github.com>
2024-05-17 18:06:13 -0400

    Update app.js
    added health check endpoint

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+// Add a new route for the health endpoint 
+app.get("/health", (req, res) => { 
+
+  res.sendStatus(200); 
+
+}); 
+
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
